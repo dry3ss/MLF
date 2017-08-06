430391783f68b1b80bb4a2da83f0371431b267cf
dry3ss <willvi31@yahoo.fr>
2017-08-06 16:56:34 +0200

    slight change .gitignore

--- machine_learning_framework/lcmake.sh
+++ machine_learning_framework/lcmake.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-c_DIR="/media/will/DonneesW/My_Neural_Network_framework/MLF/machine_learning_framework/"
-cd "$c_DIR/build_l"
+cd "build_l"
 cmake .. -G "Sublime Text 2 - Unix Makefiles"
 make
 cd ".."
